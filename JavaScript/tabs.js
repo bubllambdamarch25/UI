e015b267474769cebe1ff19f12fedfe807af7f55
Aquariax <roberthrrs8@gmail.com>
2019-03-28 18:59:22 -0400

    finishing up project

--- JavaScript/tabs.js
+++ JavaScript/tabs.js
@@ -1,9 +1,12 @@
 class Cards{
  constructor(deck){
-    this.deck = deck 
-    this.deckCard = deck
+    this.deck = deck;
+    this.showCard = document.querySelector(".cardChoice");
+    this.showCard.addEventListener('click', this.display.bind(this));
  }
- 
+ display(){
+    this.deck.classList.toggle('show');
+ }
 }
 
 
