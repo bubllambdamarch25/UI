130d955a8adbf5c2ece197b5dac200671860c20a
Aquariax <roberthrrs8@gmail.com>
2019-03-28 21:54:38 -0400

    final work

--- JavaScript/tabs.js
+++ JavaScript/tabs.js
@@ -1,11 +1,12 @@
 class Cards{
  constructor(deck){
     this.deck = deck;
-    this.showCard = document.querySelector(".cardChoice");
-    this.showCard.addEventListener('click', this.display.bind(this));
+    this.showCard = deck.querySelector(".cardChoice");
+    console.log(this.showCard)
+    this.showCard.addEventListener('click', this.play.bind(this));
  }
- display(){
-    this.deck.classList.toggle('show');
+play(){
+    this.deck.classList.toggle('coraline');
  }
 }
 
